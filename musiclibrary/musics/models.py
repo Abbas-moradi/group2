fc57ab3c1582d5eeefc20b5b33a20fab64e3c1a7
mohammad <mohammadhp858@gmail.com>
2023-06-23 15:01:09 +0330

    create song model and edit relations

--- musiclibrary/musics/models.py
+++ musiclibrary/musics/models.py
@@ -1,13 +1,26 @@
 from django.db import models
 
+
 class Artist(models.Model):
     name = models.CharField(unique=True, max_length=150)
     genre = models.CharField(max_length=50)
     bio = models.TextField()
 
+
 class Album(models.Model):
     title = models.CharField(unique=True, max_length=70)
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
     release_date = models.DateField(auto_now_add=True)
-    cover_image = models.ImageField(upload_to='media/')
+    cover_image = models.ImageField(upload_to="media/")
     description = models.TextField()
+
+
+class Song(models.Model):
+    title = models.CharField(max_length=70)
+    album = models.ManyToManyField(Album, on_delete=models.CASCADE)
+    duration = models.DurationField()
+    track_number = models.PositiveIntegerField()
+    lyrics = models.TextField()
+
+    class Meta:
+        unique_together = ("album", "track_number")
